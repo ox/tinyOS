4eb377387084a74a3d06bbb992f855fa9e4f68fb
Artem Titoulenko <owlhuntr92@gmail.com>
2009-07-10 04:22:26 -0400

    changed menu.lst in grub to tinyOS, floppy image wasn't mine

--- src/main.c
+++ src/main.c
@@ -11,7 +11,7 @@ int main(struct multiboot *mboot_ptr)
     // Initialise the screen (by clearing it)
     monitor_clear();
     // Write out a sample string
-    monitor_write("OS!");
+    monitor_write("tinyOS!");
     
     return 0;
 }
